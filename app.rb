1c7ffcee217f35f5e05388595cdfb2ffec14dbcc
Mac <mac@cubering.local>
2016-11-14 17:38:50 +0300

    Copy

--- app.rb
+++ app.rb
@@ -19,3 +19,35 @@ end
 get '/contacts' do
 erb :contacts
 end
+
+post '/visit' do 
+	@username = params[:username]
+	@phone = params[:phone]
+	@date = params[:date]
+
+	@title = 'Thank You!'
+	@message ="Dear #{@username}, we'll be waiting for you at #{@date}"
+    
+    f = File.open 'users.txt', 'a'
+    f.write "User: #{@username}, Phone: #{@phone} date_time% #{@date} \n" 
+    f.close 
+
+	erb :message
+end
+
+
+post '/contacts' do 
+	@user_mail = params[:user_email]
+	@user_message = params[:user_message]
+	
+
+	@title = 'Thank You!'
+	@message ="Your message is sent, the answer will be sent at  #{@user_mail}"
+    
+    f = File.open 'users_message.txt', 'a'
+    f.write "User: #{@user_mail}, \n message: #{@user_message} \n" 
+    f.close 
+
+	erb :message
+end
+
